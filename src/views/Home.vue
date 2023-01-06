88f393db2bbc1a2778b633ec36309dc15a69e591
limteractive <limteractive@gmail.com>
2023-01-06 16:30:11 +0900

    Add : (Home)텍스트 루프 애니메이션

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,62 @@
 
 <template>
   <div>
-    <!-- <aside class="mainTitle">mainTitlemainTitlemainTitle</aside> -->
+    <section>
+      <div class="scroll">
+        <span>HAPPY NEW YEAR&nbsp;</span>
+        <span>HAPPY NEW YEAR&nbsp;</span>
+      </div>
+    </section>
+
+    <div class="copyright"></div>
+
     <div class="cover"></div>
     <div id="work"></div>
   </div>
 </template>
 
 <style lang="scss">
-.mainTitle {
+section {
   position: absolute;
   top: 100px;
-  font-size: 3rem;
-  font-weight: 900;
+  width: 100%;
+  //   overflow-x: hidden;
+
+  .scroll {
+    width: 100%;
+    display: flex;
+
+    span {
+      font-size: 10rem;
+      white-space: nowrap;
+      font-weight: 100;
+      text-transform: uppercase;
+      animation: animate 20s linear infinite;
+
+      &:nth-child(2) {
+        animation: animate2 20s linear infinite;
+        animation-delay: -10s;
+      }
+    }
+  }
 }
+@keyframes animate {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
+@keyframes animate2 {
+  0% {
+    transform: translateX(0%);
+  }
+  100% {
+    transform: translateX(-200%);
+  }
+}
+
 .cover {
   width: 100%;
   height: 100vh;
